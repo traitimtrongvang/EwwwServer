ab176fe4860d69d249cbb96066b99a92ba57088f
traitimtrongvang <hutthuoclacohaichosuckhoe@outlook.com>
2023-07-18 16:53:26 +0700

    feat<ewwwdb>: create `UserConfig`

---
+++ Driven/EwwwDb/EntityConfig/UserConfig.cs
@@ -0,0 +1,25 @@
+﻿using Application.Entities;
+using Application.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EwwwDb.EntityConfig;
+
+public class UserConfig : EntityBaseConfig<User>
+{
+    public override void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder
+            .HasIndex(u => u.AuthId)
+            .IsUnique();
+
+        builder
+            .Property(u => u.AuthId)
+            .HasColumnType("varchar")
+            .HasConversion(
+                authId => (string)authId,
+                authIdStr => new AuthId(authIdStr));
+        
+        base.Configure(builder);
+    }
+}

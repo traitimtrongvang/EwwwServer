9ab0ff887493d0a87c4e7c6d8ec9fc0276aaf019
traitimtrongvang <hutthuoclacohaichosuckhoe@outlook.com>
2023-07-18 16:53:53 +0700

    feat<ewwwdb>: create `EwwwDbCtx`

---
+++ Driven/EwwwDb/EwwwDbCtx.cs
@@ -0,0 +1,21 @@
+﻿using Application.Contracts.Driven.EwwwDb;
+using Application.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EwwwDb;
+
+public class EwwwDbCtx : DbContext, IEwwwDbCtx
+{
+    public DbSet<User> Users { get; set; } = null!;
+    
+    public EwwwDbCtx(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(EwwwDbCtx).Assembly);
+        
+        base.OnModelCreating(modelBuilder);
+    }
+}

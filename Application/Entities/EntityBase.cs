ad721c58792b495ec7335f38ff4d6fae560ccc95
traitimtrongvang <hutthuoclacohaichosuckhoe@outlook.com>
2023-07-18 09:31:11 +0700

    feat<application>: create `EntityBase`

---
+++ Application/Entities/EntityBase.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Entities;
+
+public abstract class EntityBase
+{
+    public Guid Id { get; init; } = Guid.NewGuid();
+
+    public DateTime CreatedAt { get; init; } = DateTime.Now;
+
+    public DateTime UpdatedAt { get; set; }
+}

6fa84180fd22044077fafc473aa6c7cc37e6756e
traitimtrongvang <hutthuoclacohaichosuckhoe@outlook.com>
2023-07-18 09:34:36 +0700

    feat<application>: create `AuthId`

---
+++ Application/ValueObjects/AuthId.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Application.ValueObjects;
+
+public record AuthId
+{
+    public required string Value { get; init; }
+
+    [SetsRequiredMembers]
+    public AuthId(string value)
+    {
+        Value = value;
+    }
+
+    public static explicit operator AuthId(string authIdStr)
+        => new AuthId(authIdStr);
+};

35a768f9b76acf887d0718bc886ef4c89383cabc
traitimtrongvang <hutthuoclacohaichosuckhoe@outlook.com>
2023-07-18 16:52:54 +0700

    feat<driven>: create `EntityBaseConfig`

---
+++ Driven/EwwwDb/EntityConfig/EntityBaseConfig.cs
@@ -0,0 +1,21 @@
+﻿using Application.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EwwwDb.EntityConfig;
+
+public abstract class EntityBaseConfig<T> : IEntityTypeConfiguration<T> where T : EntityBase
+{
+    public virtual void Configure(EntityTypeBuilder<T> builder)
+    {
+        builder
+            .HasKey(e => e.Id);
+        
+        // builder
+        //     .Property(e => e.CreatedAt)
+        
+        builder
+            .Property(e => e.UpdatedAt)
+            .ValueGeneratedOnAddOrUpdate();
+    }
+}

26ce5145c3fa0dc89e0c9ef066fef3ba8e5259fa
traitimtrongvang <hutthuoclacohaichosuckhoe@outlook.com>
2023-07-18 16:21:55 +0700

    feat<startup>: create `AddEwwwDbServices`

---
+++ StartUp/Driven/EwwwDbServiceConfig.cs
@@ -0,0 +1,16 @@
+﻿using EwwwDb;
+using EwwwDb.Settings;
+using Microsoft.EntityFrameworkCore;
+
+namespace StartUp.Driven;
+
+public static class EwwwDbServiceConfig
+{
+    public static void AddEwwwDbServices(this IServiceCollection services, IConfiguration config)
+    {
+        var ewwwDbSett = config.GetSection(nameof(EwwwDbSetting)).Get<EwwwDbSetting>()!;
+        
+        services.AddDbContext<EwwwDbCtx>(
+            options => options.UseNpgsql(ewwwDbSett.ConnStr));
+    }
+}

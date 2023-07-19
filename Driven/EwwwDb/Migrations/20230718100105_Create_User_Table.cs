74af57eb80d4620fc2ae51870bfcba1f98439586
traitimtrongvang <hutthuoclacohaichosuckhoe@outlook.com>
2023-07-19 18:46:03 +0700

    feat<ewwwdb>: migrate database

---
+++ Driven/EwwwDb/Migrations/20230718100105_Create_User_Table.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EwwwDb.Migrations
+{
+    /// <inheritdoc />
+    public partial class Create_User_Table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    AuthId = table.Column<string>(type: "varchar", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_AuthId",
+                table: "Users",
+                column: "AuthId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}

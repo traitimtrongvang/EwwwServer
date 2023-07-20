1e6310cbc941dec65b740a9d0c9c64c6bc50e542
traitimtrongvang <hutthuoclacohaichosuckhoe@outlook.com>
2023-07-21 04:30:27 +0700

    feat<application>: add `FindOneByAuthIdAsync`

--- Application/Contracts/Driven/EwwwDb/Repositories/IUserRepository.cs
+++ Application/Contracts/Driven/EwwwDb/Repositories/IUserRepository.cs
@@ -1,8 +1,9 @@
 ﻿using Application.Entities;
+using Application.ValueObjects;
 
 namespace Application.Contracts.Driven.EwwwDb.Repositories;
 
 public interface IUserRepository : IRepositoryBase<User>
 {
-    
+    Task<User?> FindOneByAuthIdAsync(AuthId authId);
 }

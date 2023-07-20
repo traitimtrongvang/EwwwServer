8604a3135c5161e917164a1f063173dc70f655a9
traitimtrongvang <hutthuoclacohaichosuckhoe@outlook.com>
2023-07-21 03:55:10 +0700

    feat<application>: create `IRepositoryBase`

---
+++ Application/Contracts/Driven/EwwwDb/Repositories/IRepositoryBase.cs
@@ -0,0 +1,8 @@
+﻿using Application.Entities;
+
+namespace Application.Contracts.Driven.EwwwDb.Repositories;
+
+public interface IRepositoryBase<in T> where T : EntityBase
+{
+    Task InsertAsync(T entity);
+}

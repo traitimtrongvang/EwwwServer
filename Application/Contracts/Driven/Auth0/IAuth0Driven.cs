fe1593567a0632d9a4ea24ff52f488657f54a0d7
traitimtrongvang <hutthuoclacohaichosuckhoe@outlook.com>
2023-07-22 04:29:36 +0700

    feat<application>: create `IAuth0Driven`

---
+++ Application/Contracts/Driven/Auth0/IAuth0Driven.cs
@@ -0,0 +1,10 @@
+﻿using Application.Contracts.Driven.Auth0.Dtos;
+using Application.Contracts.Driven.Auth0.Exceptions;
+
+namespace Application.Contracts.Driven.Auth0;
+
+public interface IAuth0Driven
+{
+    /// <exception cref="UnauthorizedExc">description</exception>
+    Task<AuthorInfo> AuthorizeAndReadTokenAsync(string accessToken);
+}

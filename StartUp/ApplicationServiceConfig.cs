d93fb5617e0f958be7e1ae058b1dfcd50424f014
traitimtrongvang <hutthuoclacohaichosuckhoe@outlook.com>
2023-07-22 04:31:04 +0700

    feat<startup>: create `ApplicationServiceConfig`

---
+++ StartUp/ApplicationServiceConfig.cs
@@ -0,0 +1,13 @@
+﻿using Application.Contracts.Driven.EwwwDb;
+using EwwwDb;
+
+namespace StartUp;
+
+public static class ApplicationServiceConfig
+{
+    public static void AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddScoped<IEwwwDbCtx>(
+            sp => sp.GetRequiredService<EwwwDbCtx>());
+    }
+}

cf3d5950d847f0000d035c0b3c7e96c5b7b6ff2d
traitimtrongvang <hutthuoclacohaichosuckhoe@outlook.com>
2023-07-22 04:30:12 +0700

    feat<driven>: create `Auth0Setting`

---
+++ Driven/Auth0/Settings/Auth0Setting.cs
@@ -0,0 +1,22 @@
+﻿namespace Auth0.Settings;
+
+public class Auth0Setting
+{
+    public required string DomainUrl { get; init; }
+    public required string PublicKeyUri { get; init; }
+    public required string Issuer { get; init; }
+    public required List<string> Audiences { get; init; }
+    public string IssuerSigningKeysStr { get; private set; } = null!;
+    
+    private readonly HttpClient _httpClient;
+
+    public Auth0Setting()
+    {
+        _httpClient = new HttpClient();
+    }
+    
+    public async Task FetchIssuerSigningKeysStrAsync()
+    {
+        IssuerSigningKeysStr = await _httpClient.GetStringAsync($"{DomainUrl}/{PublicKeyUri}");
+    }
+}

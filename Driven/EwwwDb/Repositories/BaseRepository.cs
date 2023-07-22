bdb8e1c5445889597029e8afa7499e27b5547cad
traitimtrongvang <hutthuoclacohaichosuckhoe@outlook.com>
2023-07-22 14:09:44 +0700

    feat<ewwwdb>: create `BaseRepository`

---
+++ Driven/EwwwDb/Repositories/BaseRepository.cs
@@ -0,0 +1,25 @@
+﻿using Application.Contracts.Driven.EwwwDb.Repositories;
+using Application.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EwwwDb.Repositories;
+
+public abstract class BaseRepository<T> : IRepositoryBase<T> where T : EntityBase
+{
+    private protected readonly DbSet<T> DbSet;
+
+    protected BaseRepository(EwwwDbCtx ewwwDbCtx)
+    {
+        DbSet = ewwwDbCtx.Set<T>();
+    }
+
+    public async Task AddAsync(T entity)
+    {
+        await DbSet.AddAsync(entity);
+    }
+
+    public Task<T?> FindOneByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}

6d204845440eb09be12d3ebefd465a9e7eec54f6
traitimtrongvang <hutthuoclacohaichosuckhoe@outlook.com>
2023-07-22 14:09:58 +0700

    feat<ewwwdb>: create `UserRepository`

---
+++ Driven/EwwwDb/Repositories/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using Application.Contracts.Driven.EwwwDb.Repositories;
+using Application.Entities;
+using Application.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+
+namespace EwwwDb.Repositories;
+
+public class UserRepository : BaseRepository<User>, IUserRepository
+{
+    public UserRepository(EwwwDbCtx ewwwDbCtx) : base(ewwwDbCtx)
+    {
+    }
+
+    public Task<User?> FindOneByAuthIdAsync(AuthId authId)
+    {
+        return DbSet.SingleOrDefaultAsync(u => (string)u.AuthId == (string)authId);
+    }
+}

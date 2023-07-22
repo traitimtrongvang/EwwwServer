750138fbd5cc1107e0e55a5976d5d17a3ca77d61
traitimtrongvang <hutthuoclacohaichosuckhoe@outlook.com>
2023-07-22 14:08:49 +0700

    feat<application>: create `UserService.CreateUserAsync`

---
+++ Application/Services/UserService.cs
@@ -0,0 +1,31 @@
+﻿using Application.Contracts.Driven.EwwwDb.Repositories;
+using Application.Entities;
+using Application.Exceptions;
+using Application.ValueObjects;
+
+namespace Application.Services;
+
+public interface IUserService
+{
+    Task<User> CreateUserAsync(AuthId authId);
+}
+
+public class UserService : IUserService
+{
+    private readonly IUserRepository _userRepository;
+
+    public UserService(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+    
+    /// <exception cref="UserAlreadyRegisterExc"></exception>
+    public async Task<User> CreateUserAsync(AuthId authId)
+    {
+        var user = await _userRepository.FindOneByAuthIdAsync(authId);
+        if (user is not null)
+            throw new UserAlreadyRegisterExc();
+
+        return new User{AuthId = authId};
+    }
+}

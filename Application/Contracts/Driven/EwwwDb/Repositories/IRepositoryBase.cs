d31cf5299adcc751fbc6b6deb8c4c5e53b997113
traitimtrongvang <hutthuoclacohaichosuckhoe@outlook.com>
2023-07-22 14:08:57 +0700

    Update IRepositoryBase.cs

--- Application/Contracts/Driven/EwwwDb/Repositories/IRepositoryBase.cs
+++ Application/Contracts/Driven/EwwwDb/Repositories/IRepositoryBase.cs
@@ -4,6 +4,6 @@ namespace Application.Contracts.Driven.EwwwDb.Repositories;
 
 public interface IRepositoryBase<T> where T : EntityBase
 {
-    Task InsertAsync(T entity);
+    Task AddAsync(T entity);
     Task<T?> FindOneByIdAsync(Guid id);
 }

4c96aba4c2ed0d6aa67fc2aeaea94db4be68e9ce
traitimtrongvang <hutthuoclacohaichosuckhoe@outlook.com>
2023-07-22 17:10:13 +0700

    Create EwwwApiServiceConfig.cs

---
+++ StartUp/Driving/EwwwApiServiceConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+
+namespace StartUp.Driving;
+
+public static class EwwwApiServiceConfig
+{
+    public static void AddEwwwApiServices(this IServiceCollection services)
+    {
+        services.AddRouting(options => options.LowercaseUrls = true);
+        
+        services
+            .AddControllers()
+            .AddJsonOptions(
+                options => options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase)
+            .AddNewtonsoftJson(options =>
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+        
+        // services.AddJson
+        
+        services.AddEndpointsApiExplorer();
+        
+        services.AddSwaggerGen();
+    }
+}

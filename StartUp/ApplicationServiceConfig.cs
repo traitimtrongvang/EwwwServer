72d360608889a11203f229e31ff3b763f14e7349
traitimtrongvang <hutthuoclacohaichosuckhoe@outlook.com>
2023-07-22 14:10:06 +0700

    Update ApplicationServiceConfig.cs

--- StartUp/ApplicationServiceConfig.cs
+++ StartUp/ApplicationServiceConfig.cs
@@ -1,5 +1,10 @@
-﻿using Application.Contracts.Driven.EwwwDb;
+﻿using Application.Contracts.Driven.Auth0;
+using Application.Contracts.Driven.EwwwDb;
+using Application.Contracts.Driven.EwwwDb.Repositories;
+using Application.Services;
+using Auth0;
 using EwwwDb;
+using EwwwDb.Repositories;
 
 namespace StartUp;
 
@@ -7,7 +12,25 @@ public static class ApplicationServiceConfig
 {
     public static void AddApplicationServices(this IServiceCollection services)
     {
+        #region database
+
         services.AddScoped<IEwwwDbCtx>(
             sp => sp.GetRequiredService<EwwwDbCtx>());
+
+        services.AddScoped<IUserRepository, UserRepository>();
+        
+        #endregion
+
+        #region auth0
+
+        services.AddScoped<IAuth0Driven, Auth0Driven>();
+
+        #endregion
+
+        #region services
+
+        services.AddScoped<IUserService, UserService>();
+
+        #endregion
     }
 }

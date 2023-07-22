b640b7e02c15c08ef496e84a5fc2435c3ab23024
traitimtrongvang <hutthuoclacohaichosuckhoe@outlook.com>
2023-07-22 14:10:11 +0700

    Create Auth0ServiceConfig.cs

---
+++ StartUp/Driven/Auth0ServiceConfig.cs
@@ -0,0 +1,11 @@
+﻿using Auth0.Services;
+
+namespace StartUp.Driven;
+
+public static class Auth0ServiceConfig
+{
+    public static void AddAuth0Service(this IServiceCollection services)
+    {
+        services.AddScoped<ITokenService, TokenService>();
+    }
+}

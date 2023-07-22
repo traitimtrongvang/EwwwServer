aa39854097b8d5aa9fbd78a30962ba9928778f0b
traitimtrongvang <hutthuoclacohaichosuckhoe@outlook.com>
2023-07-22 13:21:43 +0700

    feat<auth0>: create `Auth0Driven`

---
+++ Driven/Auth0/Auth0Driven.cs
@@ -0,0 +1,34 @@
+﻿using Application.Contracts.Driven.Auth0;
+using Application.Contracts.Driven.Auth0.Dtos;
+using Application.Contracts.Driven.Auth0.Exceptions;
+using Auth0.Exceptions;
+using Auth0.Services;
+
+namespace Auth0;
+
+public class Auth0Driven : IAuth0Driven
+{
+    private readonly ITokenService _tokenService;
+
+    public Auth0Driven(ITokenService tokenService)
+    {
+        _tokenService = tokenService;
+    }
+
+    public AuthorInfo AuthorizeAndReadToken(string accessToken)
+    {
+        try
+        {
+            var jwtToken = _tokenService.VerifyAndReadJwtToken(accessToken);
+
+            return new AuthorInfo
+            {
+                Id = jwtToken.Payload.Sub
+            };
+        }
+        catch (InvalidTokenExc e)
+        {
+            throw new UnauthorizedExc();
+        }
+    }
+}

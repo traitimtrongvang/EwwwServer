baa1740adc76338a71b668c7dfce17e448b3c5a3
traitimtrongvang <hutthuoclacohaichosuckhoe@outlook.com>
2023-07-23 11:44:35 +0700

    feat<ewwwapi>: create `EwwwApiConfig.UseEwwwApi`

---
+++ Driving/EwwwApi/EwwwApiConfig.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+
+namespace EwwwApi;
+
+public static class EwwwApiConfig
+{
+    public static void UseEwwwApi(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+    
+        app.UseHttpsRedirection();
+        
+        app.MapControllers();
+    }
+}

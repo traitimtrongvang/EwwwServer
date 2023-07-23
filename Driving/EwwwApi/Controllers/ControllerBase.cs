1b935a94c065260d9a115ee16f4d927b5c430c23
traitimtrongvang <hutthuoclacohaichosuckhoe@outlook.com>
2023-07-23 11:43:49 +0700

    feat<ewwwapi>: create `ControllerBase`

---
+++ Driving/EwwwApi/Controllers/ControllerBase.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EwwwApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public abstract class ControllerBase: Microsoft.AspNetCore.Mvc.ControllerBase
+{
+    
+}

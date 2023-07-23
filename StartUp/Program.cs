421c273e3ffc9ea76bc89d0b9a1115af705be0bb
traitimtrongvang <hutthuoclacohaichosuckhoe@outlook.com>
2023-07-23 11:45:25 +0700

    Update Program.cs

--- StartUp/Program.cs
+++ StartUp/Program.cs
@@ -1,13 +1,23 @@
+using EwwwApi;
 using StartUp;
 using StartUp.Driven;
+using StartUp.Driving;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var config = builder.Services.BuildCustomConfiguration();
+var config = await builder.Services.BuildCustomConfigurationAsync();
 
 #region service configuration
 
+// driven
 builder.Services.AddEwwwDbServices(config);
+builder.Services.AddAuth0Service();
+
+// application
+builder.Services.AddApplicationServices();
+
+//driving
+builder.Services.AddEwwwApiServices();
 
 #endregion
 
@@ -15,34 +25,8 @@ var app = builder.Build();
 
 #region app configuration
 
-app.MapGet("/", () => "Hello World!");
+app.UseEwwwApi();
 
 #endregion
 
 app.Run();
-
-// var builder = WebApplication.CreateBuilder(args);
-//
-// // Add services to the container.
-//
-// builder.Services.AddControllers();
-// // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-// builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen();
-//
-// var app = builder.Build();
-//
-// // Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseSwagger();
-//     app.UseSwaggerUI();
-// }
-//
-// app.UseHttpsRedirection();
-//
-// app.UseAuthorization();
-//
-// app.MapControllers();
-//
-// app.Run();

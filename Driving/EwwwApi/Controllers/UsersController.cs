dfa1fb1d4b131e0fd0daabbfffe48a9f89a66b5c
traitimtrongvang <hutthuoclacohaichosuckhoe@outlook.com>
2023-07-27 06:10:34 +0700

    feat<ewwwapi>: create `UsersController.RegisterNewUserAsync`

---
+++ Driving/EwwwApi/Controllers/UsersController.cs
@@ -0,0 +1,45 @@
+﻿using Application.Contracts.Driving.Api.Dtos;
+using Application.Exceptions;
+using Application.UseCases;
+using EwwwApi.Constants;
+using EwwwApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EwwwApi.Controllers;
+
+public interface IUsersController
+{
+    public Task<IActionResult> RegisterNewUserAsync(RegisterNewUserRequest req);
+}
+
+public class UsersController : ControllerBase, IUsersController
+{
+    private readonly IRegisterNewUser _registerNewUser;
+
+    public UsersController(IRegisterNewUser registerNewUser)
+    {
+        _registerNewUser = registerNewUser;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> RegisterNewUserAsync(RegisterNewUserRequest req)
+    {
+        try
+        {
+            await _registerNewUser.HandleAsync(req);
+            
+            return Ok(new Response
+            {
+                Code = ResponseCode.Ok
+            });
+        }
+        catch (UserAlreadyRegisterExc)
+        {
+            return BadRequest(new Response
+            {
+                Code = ResponseCode.BadRequest,
+                Message = "user is already exits"
+            });
+        }
+    }
+}

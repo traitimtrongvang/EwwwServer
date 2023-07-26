a5b0d80ba2315036f16578792d0c7ceb3c21818c
traitimtrongvang <hutthuoclacohaichosuckhoe@outlook.com>
2023-07-27 06:09:59 +0700

    feat<ewwwapi>: create `Response`

---
+++ Driving/EwwwApi/Models/Response.cs
@@ -0,0 +1,14 @@
+﻿using EwwwApi.Constants;
+
+namespace EwwwApi.Models;
+
+public record Response
+{
+    public required ResponseCode Code { get; init; }
+    public string? Message { get; init; }
+}
+
+public record Response<T> : Response
+{
+    public required T Payload { get; init; }
+}

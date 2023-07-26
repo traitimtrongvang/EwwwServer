bd5093fec6108a6398861e3e0a5839034df17857
traitimtrongvang <hutthuoclacohaichosuckhoe@outlook.com>
2023-07-27 06:08:18 +0700

    feat<ewwapi>: create `ResponseCode`

---
+++ Driving/EwwwApi/Constants/ResponseCode.cs
@@ -0,0 +1,12 @@
+﻿namespace EwwwApi.Constants;
+
+public enum ResponseCode
+{
+    Ok = 200_0,
+    
+    BadRequest = 400_0,
+    
+    Unauthorized = 401_0,
+    
+    InternalServerError = 500_0
+}

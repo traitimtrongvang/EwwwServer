87dda195ba69e84d1d81ea4a1459c476c0ad9828
traitimtrongvang <hutthuoclacohaichosuckhoe@outlook.com>
2023-07-27 07:50:13 +0700

    feat<application>: create `SendMessageRequest`

---
+++ Application/Contracts/Driving/SignalR/Dtos/SendMessageRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Application.Contracts.Driving.SignalR.Dtos;
+
+// TODO implement it
+public record SendMessageRequest
+{
+    public required string ReceiverEmail { get; init; }
+    
+    public required string Message { get; init; }
+
+    [SetsRequiredMembers]
+    public SendMessageRequest(string receiverEmail, string message)
+    {
+        ReceiverEmail = receiverEmail;
+        Message = message;
+    }
+}
